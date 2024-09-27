f7548a6515c4e2350133d371bddb2015f77ea50a
BOSS-Codejs <tonali.20210647ab@gmail.com>
2024-09-27 00:54:00 -0600

    local10

--- src/components/Document/Document.js
+++ src/components/Document/Document.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Doc.css';
+import React from "react";
+import "./Doc.css";
 
 const Documentation = () => {
   return (
@@ -9,14 +9,15 @@ const Documentation = () => {
       <section className="method-section">
         <h2>Cifrado César</h2>
         <p>
-          El cifrado César es una técnica de cifrado clásica en la que cada letra
-          del texto original es desplazada un número fijo de posiciones en el alfabeto.
+          El cifrado César es una técnica de cifrado clásica en la que cada
+          letra del texto original es desplazada un número fijo de posiciones en
+          el alfabeto.
         </p>
         <h3>¿Cómo funciona?</h3>
         <p>
-          Se elige un número de desplazamiento y cada letra se reemplaza por otra que
-          está ese número de posiciones adelante en el alfabeto. Para descifrar, se
-          realiza el proceso inverso.
+          Se elige un número de desplazamiento y cada letra se reemplaza por
+          otra que está ese número de posiciones adelante en el alfabeto. Para
+          descifrar, se realiza el proceso inverso.
         </p>
         <h3>Ventajas</h3>
         <ul>
@@ -29,19 +30,23 @@ const Documentation = () => {
           <li>Vulnerable a ataques de fuerza bruta</li>
         </ul>
         <h3>Ejemplo</h3>
-        <p>Con un desplazamiento de 3, el mensaje "HOLA" se convierte en "KROD".</p>
+        <p>
+          Con un desplazamiento de 3, el mensaje "HOLA" se convierte en "KROD".
+        </p>
       </section>
 
       <section className="method-section">
         <h2>Cifrado Escítala</h2>
         <p>
-          El cifrado Escítala es una técnica de transposición utilizada en la antigua Grecia, donde
-          un mensaje se cifra enrollando una tira alrededor de un cilindro.
+          El cifrado Escítala es una técnica de transposición utilizada en la
+          antigua Grecia, donde un mensaje se cifra enrollando una tira
+          alrededor de un cilindro.
         </p>
         <h3>¿Cómo funciona?</h3>
         <p>
-          El texto se escribe en espiral alrededor del cilindro. Una vez desenrollado, el texto parece desordenado,
-          y solo se puede descifrar con un cilindro del mismo diámetro.
+          El texto se escribe en espiral alrededor del cilindro. Una vez
+          desenrollado, el texto parece desordenado, y solo se puede descifrar
+          con un cilindro del mismo diámetro.
         </p>
         <h3>Ventajas</h3>
         <ul>
@@ -54,7 +59,63 @@ const Documentation = () => {
           <li>Fácil de descifrar con herramientas modernas</li>
         </ul>
         <h3>Ejemplo</h3>
-        <p>Con un cilindro de 4 letras por vuelta, el mensaje "VEN A LAS TRES" podría cifrarse como "VENA LAS TRES".</p>
+        <p>
+          Con un cilindro de 4 letras por vuelta, el mensaje "VEN A LAS TRES"
+          podría cifrarse como "VENA LAS TRES".
+        </p>
+      </section>
+
+      <section>
+        <div className="table-container">
+          <h2
+            style={{ textAlign: "center", top: "10px", marginBottom: "10px" }}
+          >
+            Diferencia de los método de cifrado
+          </h2>
+          <table className="comparison-table">
+            <thead>
+              <tr>
+                <th>Aspecto</th>
+                <th>Cifrado César</th>
+                <th>Cifrado Escítala</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Aspecto">Diferencia técnica</td>
+                <td data-label="Cifrado César">
+                  Sustitución simple de letras basada en un desplazamiento en el
+                  alfabeto.
+                </td>
+                <td data-label="Cifrado Escítala">
+                  Cifrado de transposición basado en enrollar el mensaje en una
+                  varilla.
+                </td>
+              </tr>
+              <tr>
+                <td data-label="Aspecto">Seguridad</td>
+                <td data-label="Cifrado César">
+                  Débil, es vulnerable a ataques por fuerza bruta ya que hay
+                  solo 25 posibles combinaciones.
+                </td>
+                <td data-label="Cifrado Escítala">
+                  Moderadamente seguro, pero vulnerable a análisis de longitud
+                  de palabra o ataques físicos.
+                </td>
+              </tr>
+              <tr>
+                <td data-label="Aspecto">Complejidad</td>
+                <td data-label="Cifrado César">
+                  Fácil de implementar y descifrar.
+                </td>
+                <td data-label="Cifrado Escítala">
+                  Moderadamente complejo, depende del tamaño de la varilla y el
+                  patrón de transposición.
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </section>
     </div>
   );
--- src/components/Document/Extra.js
+++ src/components/Document/Extra.js
@@ -3,51 +3,124 @@ import "./Doc.css";
 
 const Extra = () => {
   return (
-    <div className="table-container">
-      <h2 style={{textAlign:'center',top:'10px',marginBottom:'10px'}}>Diferencia de los método de cifrado</h2>
-      <table className="comparison-table">
-        <thead>
-          <tr>
-            <th>Aspecto</th>
-            <th>Cifrado César</th>
-            <th>Cifrado Escítala</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td data-label="Aspecto">Diferencia técnica</td>
-            <td data-label="Cifrado César">
-              Sustitución simple de letras basada en un desplazamiento en el
-              alfabeto.
-            </td>
-            <td data-label="Cifrado Escítala">
-              Cifrado de transposición basado en enrollar el mensaje en una
-              varilla.
-            </td>
-          </tr>
-          <tr>
-            <td data-label="Aspecto">Seguridad</td>
-            <td data-label="Cifrado César">
-              Débil, es vulnerable a ataques por fuerza bruta ya que hay solo 25
-              posibles combinaciones.
-            </td>
-            <td data-label="Cifrado Escítala">
-              Moderadamente seguro, pero vulnerable a análisis de longitud de
-              palabra o ataques físicos.
-            </td>
-          </tr>
-          <tr>
-            <td data-label="Aspecto">Complejidad</td>
-            <td data-label="Cifrado César">
-              Fácil de implementar y descifrar.
-            </td>
-            <td data-label="Cifrado Escítala">
-              Moderadamente complejo, depende del tamaño de la varilla y el
-              patrón de transposición.
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="container">
+      <h1 style={{textAlign:'center',marginBottom:'10px'}}>Cifrado y Descifrado Escítala</h1>
+      <section className="section">
+        <h2 style={{textAlign:'center',marginBottom:'10px'}}>¿Qué es el cifrado Escítala?</h2>
+        <p style={{marginTop:'30px',marginBottom:'10px'}}>
+          El cifrado escítala es un método antiguo de cifrado que consiste en
+          escribir un mensaje en filas de un número fijo de columnas, para luego
+          leer el mensaje en columnas. En el código siguiente, el número de
+          columnas es la clave (<code>key</code>).
+        </p>
+      </section>
+
+      <section className="section">
+        <h2 style={{textAlign:'center',marginBottom:'10px'}}>Código de Cifrado</h2>
+        <p style={{marginTop:'30px',marginBottom:'10px'}}>El siguiente código implementa el cifrado de escítala en React:</p>
+        <pre>
+          <code>{`const key = parseInt(columns);
+let cipherText = "";
+
+for (let i = 0; i < key; i++) {
+  for (let j = i; j < message.length; j += key) {
+    cipherText += message[j];
+  }
+}`}</code>
+        </pre>
+        <p style={{marginTop:'30px',marginBottom:'10px'}}>
+          En este algoritmo, el número de columnas se toma como clave (
+          <code>key</code>). Luego, se itera a través de cada columna para
+          recolectar los caracteres correspondientes del mensaje original. Estos
+          caracteres se concatenan en <code>cipherText</code> para obtener el
+          mensaje cifrado.
+        </p>
+      </section>
+
+      <section className="section">
+        <h2 style={{textAlign:'center',marginBottom:'10px'}}>Código de Descifrado</h2>
+        <p style={{marginTop:'30px',marginBottom:'10px'}}>El siguiente código implementa el descifrado del texto cifrado:</p>
+        <pre>
+          <code>{`const key = parseInt(columns);
+let plainText = Array(crypt.length).fill("");
+let index = 0;
+
+for (let i = 0; i < key; i++) {
+  for (let j = i; j < crypt.length; j += key) {
+    plainText[j] = crypt[index++];
+  }                  
+}
+return plainText.join("");`}</code>
+        </pre>
+        <p style={{marginTop:'30px',marginBottom:'10px'}}>
+          En este caso, el texto cifrado se transforma de nuevo en su forma
+          original. Se distribuyen los caracteres en la matriz de texto plano (
+          <code>plainText</code>) basándose en la misma cantidad de columnas
+          utilizada en el cifrado, y luego se reconstruye el mensaje original.
+        </p>
+      </section>
+      <h1 style={{textAlign:'center',marginBottom:'10px'}}>Cifrado y Descifrado César</h1>
+
+      <section className="section">
+        <h2 style={{textAlign:'center',marginBottom:'10px'}}>¿Qué es el cifrado César?</h2>
+        <p style={{marginTop:'30px',marginBottom:'10px'}}>
+          El cifrado César es un tipo de cifrado por sustitución en el que cada
+          letra del mensaje es reemplazada por otra que se encuentra un número
+          fijo de posiciones más adelante en el alfabeto, dependiendo de un
+          desplazamiento (<code>columns</code>).
+        </p>
+      </section>
+
+      <section className="section">
+        <h2 style={{textAlign:'center',marginBottom:'10px'}}>Código de Cifrado</h2>
+        <p style={{marginTop:'30px',marginBottom:'10px'}}>El siguiente código implementa el cifrado César en React:</p>
+        <pre>
+          <code>{`let respuesta = ""; 
+let cifrado = alfabeto.slice(-columns) + alfabeto.slice(0, alfabeto.length - columns);
+
+for (let i = 0; i < message.length; i++) {
+  let letra = message[i].toLowerCase();
+  if (letra === " ") {
+    respuesta += " ";
+  } else {
+    respuesta += cifrado[alfabeto.indexOf(letra)] || letra;
+  }
+}
+return respuesta;`}</code>
+        </pre>
+        <p style={{marginTop:'30px',marginBottom:'10px'}}>
+          El código toma un mensaje y lo recorre letra por letra. Luego, ajusta
+          el alfabeto basado en el número de posiciones (<code>columns</code>) y
+          reemplaza cada letra con la que corresponde en el alfabeto cifrado. Si
+          se encuentra un espacio, lo deja sin modificar.
+        </p>
+      </section>
+
+      <section className="section">
+        <h2 tyle={{textAlign:'center',marginBottom:'10px'}}>Código de Descifrado</h2>
+        <p style={{marginTop:'30px',marginBottom:'10px'}}>El siguiente código implementa el descifrado del texto cifrado:</p>
+        <pre>
+          <code>{`const texto = message.toUpperCase(); 
+
+return texto.split("").map((caracter) => {
+  const indice = alfabeto.indexOf(caracter);
+
+  if (indice === -1) {
+    return caracter;
+  }
+
+  let nuevoIndice = (indice - columns) % alfabeto.length;
+  if (nuevoIndice < 0) nuevoIndice += alfabeto.length;
+
+  return alfabeto[nuevoIndice];
+}).join("");`}</code>
+        </pre>
+        <p style={{marginTop:'30px',marginBottom:'10px'}}>
+          Este código toma el mensaje cifrado y lo transforma nuevamente al
+          texto original recorriendo el alfabeto en sentido inverso, utilizando
+          el mismo desplazamiento (<code>columns</code>).
+        </p>
+      </section>
     </div>
   );
 };
